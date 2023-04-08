a171d933a8379ca8f14b67e20488ac6f0a9f3bcb
lichty <joshua.m.lichty@gmail.com>
2023-04-08 10:03:28 -0600

    Config fixes Package updates Changed entrypoint

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ RUN apk add --no-cache libc6-compat
 WORKDIR /app
 
 # Install dependencies based on the preferred package manager
-COPY package.json yarn.lock* package-lock.json* pnpm-lock.yaml* ./
+COPY package.json yarn.lock* ./
 RUN yarn --frozen-lockfile;
 
 # Rebuild the source code only when needed
@@ -14,22 +14,15 @@ WORKDIR /app
 COPY --from=deps /app/node_modules ./node_modules
 COPY . .
 
-# Next.js collects completely anonymous telemetry data about general usage.
-# Learn more here: https://nextjs.org/telemetry
-# Uncomment the following line in case you want to disable telemetry during the build.
 ENV NEXT_TELEMETRY_DISABLED 1
 
 RUN yarn build
-
-# If using npm comment out above and use below instead
-# RUN npm run build
 
 # Production image, copy all the files and run next
 FROM node:18-alpine3.15 AS runner
 WORKDIR /app
 
 ENV NODE_ENV production
-# Uncomment the following line in case you want to disable telemetry during runtime.
 ENV NEXT_TELEMETRY_DISABLED 1
 
 RUN addgroup --system --gid 1001 nodejs
@@ -37,9 +30,6 @@ RUN adduser --system --uid 1001 nextjs
 RUN apk add --no-cache nano
 
 COPY --from=builder /app/public ./public
-
-# Automatically leverage output traces to reduce image size
-# https://nextjs.org/docs/advanced-features/output-file-tracing
 COPY --from=builder --chown=nextjs:nodejs /app/.next/standalone ./
 COPY --from=builder --chown=nextjs:nodejs /app/.next/static ./.next/static
 
@@ -60,4 +50,3 @@ EXPOSE 4431
 ENV PORT 4431
 
 RUN chmod +x /app/init.sh
-# CMD ["/bin/sh", "/app/init.sh"]
--- server.js
+++ server.js
@@ -1,23 +1,205 @@
-const { createServer } = require('https');
-const { parse } = require('url');
-const { readFileSync } = require('fs');
-const next = require('next');
+process.env.NODE_ENV = 'production';
+process.chdir(__dirname);
+const NextServer = require('next/dist/server/next-server').default;
+const path = require('path');
+const https = require('https');
+const { existsSync, readFileSync, writeFileSync, appendFileSync } = require('fs');
+const keyFileName = './ssl/key.pem';
+const certFileName = './ssl/cert.pem';
+const logFile = 'log.txt';
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const log = (msg, level = '') => {
+  if (!existsSync(logFile)) {
+    writeFileSync(logFile, '');
+  }
+  let output = `${level?`${level}: `:''}${msg}`;
+  switch (level) {
+    case 'Error':
+      console.error(log);
+      break;
+    case 'Info':
+      console.info(msg);
+      break;
+    default:
+      console.log(msg);
+  }
+  appendFileSync(logFile, `${new Date().toLocaleDateString()}\t${output}\n`);
+}
 
-const sslConfig = {
-  key: readFileSync('/app/ssl/key.pem'),
-  cert: readFileSync('/app/ssl/cert.pem'),
+const logError = (msg) => {
+  log(msg, 'Error');
+}
+
+const logInfo = (msg) => {
+  log(msg, 'Info');
+}
+
+// Make sure commands gracefully respect termination signals (e.g. from Docker)
+// Allow the graceful termination to be manually configurable
+if (!process.env.NEXT_MANUAL_SIG_HANDLE) {
+  process.on('SIGTERM', () => process.exit(0));
+  process.on('SIGINT', () => process.exit(0));
+}
+
+const currentPort = parseInt(process.env.PORT, 10) || 3000;
+
+logInfo('Checking SSL files...');
+
+let filesExist = true;
+// Check that the file exists locally
+const checkFile = (file) => {
+  let result = true;
+  if (!existsSync(file)) {
+    result = false;
+    logError(`${file} not found`);
+  }
+  return result;
+}
+
+filesExist = checkFile(keyFileName) && filesExist;
+filesExist = checkFile(certFileName) && filesExist;
+
+let key;
+let cert;
+
+if (filesExist) {
+  logInfo('Loading SSL files...');
+  key = readFileSync(keyFileName);
+  cert = readFileSync(certFileName);
+} else {
+  logError("Missing files. Aborting.");
+  throw new Error("Missing files. Aborting.");
+}
+
+logInfo('SSL files loaded');
+
+let options = {
+  key: key,
+  cert: cert
 };
 
-app.prepare().then(() => {
-  createServer(sslConfig, (req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  }).listen(process.env.PORT, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on https://localhost:${process.env.PORT}`);
-  });
-});
+let handler;
+
+logInfo('Creating server');
+
+const server = https.createServer(options, async (req, res) => {
+  try {
+    await handler(req, res);
+  } catch (err) {
+    logError(err);
+    res.statusCode = 500;
+    res.end('internal server error');
+  }
+})
+
+logInfo('Server created');
+
+try {
+  logInfo('Attempt to listen');
+  server.listen(currentPort, err => {
+    if (err) {
+      logError("Failed to start server");
+      logError(err);
+      process.exit(1);
+    }
+    logInfo('Starting server...');
+    const nextServer = new NextServer({
+      hostname: 'localhost',
+      port: currentPort,
+      dir: path.join(__dirname),
+      dev: false,
+      customServer: false,
+      conf: {
+        "env": {}, "webpackDevMiddleware": null, "eslint": { "ignoreDuringBuilds": false },
+        "typescript": {
+          "ignoreBuildErrors": false,
+          "tsconfigPath": "tsconfig.json"
+        },
+        "distDir": "./.next",
+        "cleanDistDir": true,
+        "assetPrefix": "",
+        "configOrigin": "next.config.js",
+        "useFileSystemPublicRoutes": true,
+        "generateEtags": true,
+        "pageExtensions": ["tsx", "ts", "jsx", "js"],
+        "target": "server",
+        "poweredByHeader": true,
+        "compress": true,
+        "analyticsId": "",
+        "images": {
+          "deviceSizes": [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+          "imageSizes": [16, 32, 48, 64, 96, 128, 256, 384],
+          "path": "/_next/image",
+          "loader": "default",
+          "domains": [],
+          "disableStaticImages": false,
+          "minimumCacheTTL": 60,
+          "formats": ["image/webp"],
+          "dangerouslyAllowSVG": false,
+          "contentSecurityPolicy": "script-src 'none';frame-src 'none'; sandbox; "
+        },
+        "devIndicators": {
+          "buildActivity": true,
+          "buildActivityPosition": "bottom - right"
+        },
+        "onDemandEntries": { "maxInactiveAge": 15000, "pagesBufferLength": 2 },
+        "amp": { "canonicalBase": "" },
+        "basePath": "",
+        "sassOptions": {},
+        "trailingSlash": false,
+        "i18n": null,
+        "productionBrowserSourceMaps": false,
+        "optimizeFonts": true,
+        "excludeDefaultMomentLocales": true,
+        "serverRuntimeConfig": {},
+        "publicRuntimeConfig": {},
+        "reactStrictMode": true,
+        "httpAgentOptions": { "keepAlive": true },
+        "outputFileTracing": true,
+        "staticPageGenerationTimeout": 60,
+        "swcMinify": false,
+        "output": "standalone",
+        "experimental": {
+          "optimisticClientCache": true,
+          "manualClientBasePath": false,
+          "legacyBrowsers": true,
+          "browsersListForSwc": false,
+          "newNextLinkBehavior": false,
+          "cpus": 1,
+          "sharedPool": true,
+          "profiling": false,
+          "isrFlushToDisk": true,
+          "workerThreads": false,
+          "pageEnv": false,
+          "optimizeCss": false,
+          "nextScriptWorkers": false,
+          "scrollRestoration": false,
+          "externalDir": false,
+          "disableOptimizedLoading": false,
+          "gzipSize": true,
+          "swcFileReading": true,
+          "craCompat": false,
+          "esmExternals": true,
+          "appDir": false,
+          "isrMemoryCacheSize": 52428800,
+          "serverComponents": false,
+          "fullySpecified": false,
+          "outputFileTracingRoot": "",
+          "images": { "remotePatterns": [] },
+          "swcTraceProfiling": false,
+          "forceSwcTransforms": false,
+          "largePageDataBytes": 128000,
+          "trustHostHeader": false
+        },
+        "configFileName": "next.config.js"
+      },
+    })
+    logInfo("Server started");
+
+    handler = nextServer.getRequestHandler();
+
+    logInfo(`Listening on port ${currentPort}`);
+  })
+} catch (err) {
+  logError(err);
+}
